3fa7f310694e667d11438cd5fd8b6dadf38d7d35
FranciscoPoli <124818536+FranciscoPoli@users.noreply.github.com>
2024-02-16 12:04:42 -0300

    v2

--- subdivxweb.py
+++ subdivxweb.py
@@ -1,12 +1,11 @@
 import requests
-from bs4 import BeautifulSoup
 import concurrent.futures
 import streamlit as st
 from streamlit_extras.colored_header import colored_header
-
+import json
 
 results = []
-links = []
+args = []
 
 
 def showResults(array):
@@ -22,34 +21,43 @@ def highlightGreen(key1, key2, result_text):
     if key1 == '' and key2 == '':
         highlighted_text = result_text
     elif key1 == '':
-        highlighted_text = result_text.replace(f"{key_word2}",f":green[{key_word2}]")
+        highlighted_text = result_text.replace(f"{key_word2}", f":green[{key_word2}]")
     elif key2 == '':
-        highlighted_text = result_text.replace(f"{key_word}",f":green[{key_word}]")
+        highlighted_text = result_text.replace(f"{key_word}", f":green[{key_word}]")
     else:
-        highlighted_text = result_text.replace(f"{key_word}",f":green[{key_word}]").replace(f"{key_word2}",f":green[{key_word2}]")
-    
+        highlighted_text = result_text.replace(f"{key_word}", f":green[{key_word}]").replace(f"{key_word2}",
+                                                                                             f":green[{key_word2}]")
+
     return highlighted_text
-            
-        
-def searchComments(url):
+
+
+def searchComments(id, movie_title, download):
     global key_word
     global key_word2
     global test
     global test2
-    
-    r = requests.get(url)
-    soup2 = BeautifulSoup(r.text, 'html.parser')
-    comments = soup2.find_all('div', id='detalle_reng_coment1')
-    title = soup2.find_all('div', id='menu_titulo_buscador')[0].find('b').find('span').text
-    for item in comments:
-        comment = item.text.lower()
+
+    header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                            'Chrome/121.0.0.0 Safari/537.36'
+              }
+    payload = {'getComentarios': id
+               }
+
+    response = requests.post(f'https://subdivx.com/inc/ajax.php', data=payload, headers=header)
+    data = json.loads(response.text)
+
+    for item in data['aaData']:
+        comment = item['comentario'].lower()
+
         if key_word in comment and key_word2 in comment:
-            downloads = int(str(soup2.find_all('div', id='detalle_datos')[0].find_all('span')[2].text).replace(',', ''))
+            downloads = download
+            title = movie_title
+            url = f'https://subdivx.com/descargar.php?id={id}_blank'
             green_comment = highlightGreen(key_word, key_word2, comment)
-            
+
             results.append([downloads, url, green_comment, title])
             break
-            
+
         if key_word in comment:
             test = True
         if key_word2 in comment:
@@ -64,7 +72,10 @@ colored_header(
     color_name="violet-70",
 )
 
-columna1, columna2 = st.columns([1,1.5], gap="medium")
+columna1, columna2 = st.columns([1, 1.5], gap="medium")
+
+if 'title' not in st.session_state:
+    st.session_state.title = None
 
 with columna1:
     form = st.form(key='Buscar')
@@ -74,66 +85,83 @@ with columna1:
         key_word2 = st.text_input('Palabra clave 2 (p. ej. argenteam): ').lower()
         form.form_submit_button('Buscar')
 
-header = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                        'Chrome/109.0.0.0 Safari/537.36'
+header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                        'Chrome/121.0.0.0 Safari/537.36'
           }
 
-payload = {'buscar2': movie,
-           'accion': '5',
-           'masdesc': '',
-           'subtitulos': '1',
-           'realiza_b': '1'
+payload = {'buscar': movie,
+           'tabla': 'resultados',
+
            }
 
 if movie:
-    response = requests.post(f'https://www.subdivx.com/index.php', data=payload, headers=header)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    if 'extracted' not in st.session_state:
+        st.session_state.extracted = []
 
-    descriptions = soup.find_all('div', id='buscador_detalle_sub')
-    if not descriptions:
-        st.warning(f'Subdivx no encuentra resultados para esa película o serie')
-        st.stop()
+    if st.session_state.title != movie:
+        st.session_state.extracted = []
+        st.session_state.title = movie
 
-    results.clear()
+    if st.session_state.extracted == []:
+        response = requests.post(f'https://subdivx.com/inc/ajax.php', data=payload, headers=header)
+        data = json.loads(response.text)
 
-    count = 0
-    movie_link = ''
+        extracted_data = []
+        for item in data['aaData']:
+            extracted_item = {
+                'id': item['id'],
+                'descripcion': item['descripcion'],
+                'descargas': item['descargas'],
+                'titulo': item['titulo']
+            }
+            extracted_data.append(extracted_item)
+
+        st.session_state.extracted = extracted_data
+
+        if  st.session_state.extracted == []:
+            st.warning(f'Subdivx no encuentra resultados para esa película o serie')
+            st.stop()
+
+        results.clear()
+
     test = False
     test2 = False
-    for item in descriptions:
-        description = item.text.lower()
+    movie_link = ""
+    for item in st.session_state.extracted:
+        description = item['descripcion'].lower()
         if key_word in description and key_word2 in description:
-            movie_link = soup.find_all('a', {'class': 'titulo_menu_izq'}, href=True)[count]['href']
-            movie_title = soup.find_all('a', {'class': 'titulo_menu_izq'}, href=True)[count].text
-            downloads = int(str(soup.find_all('div', id='buscador_detalle_sub_datos')[count].find('b').next_sibling).replace(',', ''))
+            movie_link = f'https://subdivx.com/descargar.php?id={item["id"]}_blank'
+            movie_title = item['titulo']
+            downloads = item['descargas']
             green_description = highlightGreen(key_word, key_word2, description)
-            
+
             results.append([downloads, movie_link, green_description, movie_title])
-        
+
         if key_word in description:
             test = True
         if key_word2 in description:
             test2 = True
 
-        count += 1
 
     results.sort(key=lambda x: int(-x[0]))
-    
+
     if len(results) > 0:
         with columna2:
             showResults(results)
 
     elif not movie_link:
-        all_links = soup.find_all('a', {'class': 'titulo_menu_izq'}, href=True)
-        for item in all_links:
-            url = item['href']
-            links.append(url)
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            executor.map(searchComments, links)
+        for item in st.session_state.extracted:
+            id = item['id']
+            movie_title = item['titulo']
+            downloads = item['descargas']
+            args.append((id, movie_title, downloads))
 
-        links.clear()
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            executor.map(searchComments, *zip(*args))
+
+        args.clear()
         results.sort(key=lambda x: int(-x[0]))
-        
+
         if len(results) > 0:
             with columna2:
                 showResults(results)
@@ -141,10 +169,20 @@ if movie:
     if len(results) == 0:
         if test is False and test2 is True:
             st.warning(f'La palabra "{key_word}" no se pudo encontrar')
+            st.stop()
         elif test is True and test2 is False:
             st.warning(f'La palabra "{key_word2}" no se pudo encontrar')
+            st.stop()
         elif test is False and test2 is False:
             st.warning(f'Ninguna palabra se pudo encontrar')
+            st.stop()
         else:
             st.warning(f'Las 2 palabras existen pero no en la misma frase.  Intenta buscando solo una de ellas')
-      
+            st.stop()
+
+    elif len(results) == 1:
+        with columna1:
+            st.success(f"{len(results)} resultado encontrado")
+    else:
+        with columna1:
+            st.success(f"{len(results)} resultados encontrados")
